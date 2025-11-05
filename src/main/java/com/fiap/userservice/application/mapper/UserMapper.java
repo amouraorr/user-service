93d81b8aaa9470dcdd7888b806e1d3cfd20421db
Alex Mourão <amouraorr@yahoo.com.br>
2025-11-05 04:48:16 -0300

    implementações e ajustes

--- src/main/java/com/fiap/userservice/application/mapper/UserMapper.java
+++ src/main/java/com/fiap/userservice/application/mapper/UserMapper.java
@@ -2,8 +2,12 @@ package com.fiap.userservice.application.mapper;
 
 import com.fiap.userservice.application.dto.UserDTO;
 import com.fiap.userservice.domain.model.User;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface UserMapper {
 
     UserDTO toDTO(User user);
+
+    User toEntity(UserDTO dto);
 }
--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,4 +1,7 @@
 -- V1__create_users_table.sql
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
      CREATE TABLE users (
        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
        username VARCHAR(100) NOT NULL UNIQUE,
